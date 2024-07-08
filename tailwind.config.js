0ba957b025e703e98fec652b28b6f9adeba236ec
Nameless146 <mrnameless44@gmail.com>
2024-07-08 23:32:50 +0600

    full project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['*'],
+  theme: {
+    extend: {
+      textUnderlineOffset: {
+        14: '14px',
+      },
+    height: {
+      130: '130px',
+    },
+    }
+  },
+  plugins: [],
+}
+
